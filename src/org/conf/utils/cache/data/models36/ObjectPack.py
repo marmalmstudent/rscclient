b9ce9c47bd5d1071ee6c1e3cc5bde13c7d9d4fac
marmalmstudent <marmalm@student.chalmers.se>
2017-04-06 03:15:26 +0200

    started working on a script that assembles th .ob3 files into models36.jag. Need to write a script to generate the header for this

--- src/org/conf/utils/cache/data/models36/ObjectPack.py
+++ src/org/conf/utils/cache/data/models36/ObjectPack.py
@@ -1,4 +1,5 @@
 import numpy as np
+import os
 
 
 def str_hash(s):
@@ -11,10 +12,14 @@ def str_hash(s):
 
 if __name__ == "__main__":
     max_files = 500
-    with open("models36.jag", wb) as d:
+    with open("models36.jag", "wb") as d:
+        with open("header", "rb") as h:
+            # TODO: write a script that generates the header instead
+            # of using the old.
+            d.write(h.read())
         for i in range(0, max_files):
             if (os.path.isfile("object"+str(i)+".ob3")):
-                print("Processing file %d of %d (%.1f%%)" % (i, max_files,
-                                                             100*(i)/max_files))
-                    with open("object"+str(i)+".ob3", "rb") as s:
-                        
+                print("Processing file %d of %d (%.1f%%)"
+                      % (i, max_files, 100*(i)/max_files))
+                with open("object"+str(i)+".ob3", "rb") as s:
+                    d.write(s.read())
